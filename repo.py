3c96a3f7d137766d072b19ec00d8eda19d377e40
TheIdioticDev <168137332+theidioticdev@users.noreply.github.com>
2026-02-03 21:01:45 +0200

    Add files via upload

---
+++ repo.py
@@ -0,0 +1,29 @@
+# PenguWarp Package Repository
+# Lists all available packages that can be installed
+
+AVAILABLE_PACKAGES = {
+    "snake": {
+        "name": "Snake Game",
+        "file": "snake.py",
+        "description": "Classic snake game in terminal",
+        "version": "1.0"
+    },
+    "cowsay": {
+        "name": "Cowsay",
+        "file": "cowsay.py",
+        "description": "Make a cow say things",
+        "version": "1.0"
+    },
+    "matrix": {
+        "name": "Matrix Rain",
+        "file": "matrix.py",
+        "description": "Cool matrix falling text effect",
+        "version": "1.0"
+    },
+    "todo": {
+        "name": "Todo List",
+        "file": "todo.py",
+        "description": "Simple todo list manager",
+        "version": "1.0"
+    }
+}
